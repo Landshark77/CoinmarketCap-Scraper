857a876c99a085d0c44cea9932b60d26868f45a3
Landshark77 <github@rebelfever.net>
2021-02-14 11:06:44 -0600

    bug fixes

--- cmp_scrape.py
+++ cmp_scrape.py
@@ -12,13 +12,14 @@ timestamp = []
 name = []
 symbol = []
 slug = []
-category = []
 price = []
 volcap = []
 
+intCoinCount = 0
+
 #Load coins
-for x in range(41):
-	print(f'Retrieving Coin list page {i} of {range(2)}')
+for x in range(1, 42):
+	print(f'Building Coin list page {x} of 41')
 	if x == 1: 
 		cmc = requests.get('https://coinmarketcap.com/')
 	else:
@@ -29,29 +30,42 @@ for x in range(41):
 	coin_data = json.loads(data.contents[0])
 	listings = coin_data['props']['initialState']['cryptocurrency']['listingLatest']['data']
 	for i in listings:
-		coins[str(i['id'])] = i['slug']
+#		coins[str(i['id'])] = i['slug']
+		if i['quote']['USD']['price'] < 5 and i['quote']['USD']['market_cap'] > 0:
+			if (i['quote']['USD']['volume_24h'] / i['quote']['USD']['market_cap'] >= .1) and (i['quote']['USD']['volume_24h'] / i['quote']['USD']['market_cap'] <= .5)
+			market_cap.append(i['quote']['USD']['market_cap'])
+			volume.append(i['quote']['USD']['volume_24h'])
+			slug.append(i['slug'])
+			name.append(i['name'])
+			symbol.append(i['symbol'])
+			price.append(i['quote']['USD']['price'])
+			volcap.append(i['quote']['USD']['volume_24h'] / i['quote']['USD']['market_cap']) 
+
 
 #get data for each coin
-for i in coins:
-	page = requests.get(f'https://coinmarketcap.com/currencies/{coins[i]}/historical-data/')
-	print(f'Retrieving stats {i} of {len(coins)} -- {coins[i]}')
-	time.sleep(1)
-	soup = BeautifulSoup(page.content, 'html.parser')
-	data = soup.find('script', id="__NEXT_DATA__",type="application/json")
-	coin_data = json.loads(data.contents[0])
-	quotes = coin_data['props']['initialProps']['pageProps']['info']
-	if (quotes['status'] == "active") and (quotes['statistics']['volumeYesterday'] / quotes['statistics']['marketCap'] >= 0.1) and \
-	   (quotes['statistics']['volumeYesterday'] / quotes['statistics']['marketCap'] <= 0.5) and (quotes['statistics']['price'] <= 5 ):
-		market_cap.append(quotes['statistics']['marketCap'])
-		volume.append(quotes['statistics']['volumeYesterday'])
-		slug.append(coins[i])
-		name.append(quotes['name'])
-		symbol.append(quotes['symbol'])
-		category.append(quotes['category'])
-		price.append(quotes['statistics']['price'])
-		volcap.append(quotes['statistics']['volumeYesterday'] / quotes['statistics']['marketCap']) 
+#for i in coins:
+#	intCoinCount += 1
+#	page = requests.get(f'https://coinmarketcap.com/currencies/{coins[i]}/historical-data/')
+#	print(f'Retrieving stats {intCoinCount} of {len(coins)} for >> {coins[i]}')
+#	time.sleep(1) #must sleep to avoid throttling by website.  increase as needed.
+#	soup = BeautifulSoup(page.content, 'html.parser')
+#	data = soup.find('script', id="__NEXT_DATA__",type="application/json")
+#	coin_data = json.loads(data.contents[0])
+#	quotes = coin_data['props']['initialProps']['pageProps']['info']
+#	if (quotes['status'] == "active") and (quotes['statistics']['price'] < 5):
+#		market_cap.append(quotes['statistics']['marketCap'])
+#		volume.append(quotes['statistics']['volumeYesterday'])
+#		slug.append(coins[i])
+#		name.append(quotes['name'])
+#		symbol.append(quotes['symbol'])
+#		category.append(quotes['category'])
+#		price.append(quotes['statistics']['price'])
+#		if quotes['statistics']['marketCap'] > 0:
+#			volcap.append(quotes['statistics']['volumeYesterday'] / quotes['statistics']['marketCap']) 
+#		else:
+#			volcap.append(0)
 
-df = pd.DataFrame(columns = ['slug', 'name', 'symbol','price', 'MarketCap', 'volume','Volume / MarketCap', 'category'])
+df = pd.DataFrame(columns = ['slug', 'name', 'symbol','price', 'MarketCap', 'volume','Volume / MarketCap'])
 df['slug'] = slug
 df['name'] = name
 df['symbol'] = symbol
@@ -59,5 +73,4 @@ df['price'] = price
 df['MarketCap'] = market_cap
 df['volume'] = volume
 df['Volume / MarketCap'] = volcap 
-df['category'] = category
 df.to_csv('cmp_out.csv',index = False)
