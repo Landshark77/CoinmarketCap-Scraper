121192874e9b9ac7074bf35064b9b4c2bdb7431f
Landshark77 <github@rebelfever.net>
2021-05-16 08:22:10 -0500

    Update cmp_scrape.py

--- cmp_scrape.py
+++ cmp_scrape.py
@@ -8,13 +8,15 @@ market_cap = []
 volume = []
 name = []
 symbol = []
-slug = []
+slug = [] 
 price = []
 volcap = []
+cirsupply = []
+maxsupply = []
 
 #Load coins
-for x in range(1, 42):
-	print(f'Gathering Coin data from page {x} of 41')
+for x in range(1, 53):
+	print(f'Gathering Coin data from page {x} of 52')
 	cmc = requests.get(f'https://coinmarketcap.com/?page={x}')
 	time.sleep(0.5)
 	soup = BeautifulSoup(cmc.content, 'html.parser')
@@ -22,8 +24,9 @@ for x in range(1, 42):
 	coin_data = json.loads(data.contents[0])
 	listings = coin_data['props']['initialState']['cryptocurrency']['listingLatest']['data']
 	for i in listings:
-		if i['quote']['USD']['price'] < 5 and i['quote']['USD']['marketCap'] > 0 and i['quote']['USD']['price'] >= 0.01 and i['quote']['USD']['volume24h'] >= 1000000:
-			if (i['quote']['USD']['volume24h'] / i['quote']['USD']['marketCap'] >= .1) and (i['quote']['USD']['volume24h'] / i['quote']['USD']['marketCap'] <= .5):
+		#if i['quote']['USD']['price'] < 5 and i['quote']['USD']['marketCap'] > 0 and i['quote']['USD']['price'] >= 0.01 and i['quote']['USD']['volume24h'] >= 1000000:
+		if i['quote']['USD']['marketCap'] > 0 and i['quote']['USD']['price'] >= 0.01: #and i['quote']['USD']['volume24h'] >= 1000000:
+			#if (i['quote']['USD']['volume24h'] / i['quote']['USD']['marketCap'] >= .1) and (i['quote']['USD']['volume24h'] / i['quote']['USD']['marketCap'] <= .5):
 				market_cap.append(i['quote']['USD']['marketCap'])
 				volume.append(i['quote']['USD']['volume24h'])
 				slug.append(i['slug'])
@@ -31,13 +34,21 @@ for x in range(1, 42):
 				symbol.append(i['symbol'])
 				price.append(i['quote']['USD']['price'])
 				volcap.append(i['quote']['USD']['volume24h'] / i['quote']['USD']['marketCap']) 
+				cirsupply.append(i['circulatingSupply'])
+				
+				try:
+					maxsupply.append(i['maxSupply'])
+				except KeyError:
+					maxsupply.append(0)
 
-df = pd.DataFrame(columns = ['slug', 'name', 'symbol','price', 'MarketCap', 'volume','Volume / MarketCap'])
+df = pd.DataFrame(columns = ['slug', 'name', 'symbol','price', 'MarketCap', 'volume','Volume / MarketCap','Circulating Supply','Max Supply'])
 df['slug'] = slug
 df['name'] = name
 df['symbol'] = symbol
 df['price'] = price
 df['MarketCap'] = market_cap
 df['volume'] = volume
-df['Volume / MarketCap'] = volcap 
+df['Volume / MarketCap'] = volcap
+df['Circulating Supply'] = cirsupply
+df['Max Supply'] = maxsupply
 df.to_csv('cmp_out.csv',index = False)
