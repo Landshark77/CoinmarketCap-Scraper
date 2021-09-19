34e3ce93dd5e52cc5c9cd48d04c1e46e31a941db
Landshark77 <github@rebelfever.net>
2021-09-19 13:22:30 -0500

    Update cmp_scrape.py

--- cmp_scrape.py
+++ cmp_scrape.py
@@ -5,7 +5,7 @@ import json
 import time
 
 #Update variable to match total number of pages found on coinmarketcap
-numPages = 67
+numPages = 67 + 1
 
 #objectes for pandas
 market_cap = []
@@ -18,20 +18,13 @@ volcap = []
 cirsupply = []
 maxsupply = []
 totsupply = []
+pageNum = []
 
-#variables for array
-varName = 15
-varMarketCap = 55
-var24hrVolume = 66
-varSlug = 125
-varSymbol = 126
-varMaxSupply = 14
-varTotalSupply = 127
-varCirSupply = 2
-varPrice = 64
+df = pd.DataFrame(columns = ['PageNum', 'slug', 'name', 'symbol','price', 'MarketCap', 'volume','Volume / MarketCap','Circulating Supply','Max Supply','Total Supply'])
 
 #Load coins
 for x in range(1, numPages):
+	
 	print(f'Gathering Coin data from page {x} of {numPages-1}')
 	cmc = requests.get(f'https://coinmarketcap.com/?page={x}')
 	time.sleep(0.5)
@@ -40,24 +33,44 @@ for x in range(1, numPages):
 	coin_data = json.loads(data.contents[0])
 	listings = coin_data['props']['initialState']['cryptocurrency']['listingLatest']['data']
 	for i in listings:
+		if type(i) == dict:
+			varName = i['keysArr'].index('name')
+			varMarketCap = i['keysArr'].index('quote.USD.marketCap')
+			var24hrVolume = i['keysArr'].index('quote.USD.volume24h')
+			varSlug = i['keysArr'].index('slug')
+			varSymbol = i['keysArr'].index('symbol')
+			
+			try:
+				varMaxSupply = i['keysArr'].index('maxSupply')
+			except ValueError:
+				varMaxSupply = -1
+
+			varTotalSupply = i['keysArr'].index('totalSupply')
+			varCirSupply = i['keysArr'].index('circulatingSupply')
+			varPrice = i['keysArr'].index('quote.USD.price')
+
 		if type(i) == list: 
-			if i[varMarketCap] > 0: #this will export all coins with a marketcap greater than zero
-				market_cap.append(i[varMarketCap])
-				volume.append(i[var24hrVolume])
-				slug.append(i[varSlug])
-				name.append(i[varName])
-				symbol.append(i[varSymbol])
-				price.append(i[varPrice])
+			pageNum.append(x)
+			#if int(i[varMarketCap]) > 0: #this will export all coins with a marketcap greater than zero
+			market_cap.append(i[varMarketCap])
+			volume.append(i[var24hrVolume])
+			slug.append(i[varSlug])
+			name.append(i[varName])
+			symbol.append(i[varSymbol])
+			price.append(i[varPrice])
+			
+			try:
 				volcap.append(i[var24hrVolume] / i[varMarketCap]) 
-				cirsupply.append(i[varCirSupply])
-				totsupply.append(i[varTotalSupply])
+			except ZeroDivisionError:
+				volcap.append(0)
+				
+			cirsupply.append(i[varCirSupply])
+			totsupply.append(i[varTotalSupply])
+			
+			if varMaxSupply > 0:
+				maxsupply.append(i[varMaxSupply])
 
-				try:
-					maxsupply.append(i[varMaxSupply])
-				except KeyError:
-					maxsupply.append(0)
-
-df = pd.DataFrame(columns = ['slug', 'name', 'symbol','price', 'MarketCap', 'volume','Volume / MarketCap','Circulating Supply','Max Supply','Total Supply'])
+df['PageNum'] = pageNum
 df['slug'] = slug
 df['name'] = name
 df['symbol'] = symbol
